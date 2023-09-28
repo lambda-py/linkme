e32d1e7f133d9b8ad70b9b0c7c1f31479719ad15
Vitalii Hrebennikov <webasman@gmail.com>
2023-09-28 16:57:45 +0300

    add shared fastapi instance

---
+++ shared/common_python/contrib/fastapi/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+class SharedFastAPI(FastApi):
+    """
+    A FastAPI application that can be used by multiple services.
+
+    This is useful for sharing common FastAPI functionality across multiple
+    services. For example, if you want to share a common health check endpoint
+    across multiple services, you can do so by creating a SharedFastAPI
+    instance and then adding the health check endpoint to it.
+    """
+
+    ...
