bce80704b5f0153afe66dae51e74922caab87644
Vitalii Hrebennikov <webasman@gmail.com>
2023-10-03 18:57:05 +0300

    create profile service structure

--- profile_service/src/config/__init__.py
+++
@@ -1,5 +0,0 @@
-from profile_service.config.settings import Settings
-
-
-def get_settings() -> Settings:
-    return Settings()
--- profile_service/src/config/settings.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    service_name: str = "profile_service"
-    api_version: str = "0.1.0"
