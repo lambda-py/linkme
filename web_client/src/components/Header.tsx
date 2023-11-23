93f37adee626b7d046ed9c53669237140023f9ad
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-23 17:40:43 +0200

    refactor

---
+++ web_client/src/components/Header.tsx
@@ -0,0 +1,30 @@
+import {Link} from 'react-router-dom';
+import Logo from './Logo';
+
+type Props = {
+  heading: string,
+  paragraph: string,
+  linkName: string,
+  linkUrl: string
+}
+
+const Header = ({heading, paragraph, linkName, linkUrl}: Props) => {
+    return(
+      <div className="mb-10">
+          <div className="flex justify-center">
+            <Logo />
+          </div>
+          <h2 className="mt-6 text-center text-lg font-extrabold text-gray-900">
+              {heading}
+          </h2>
+          <p className="text-center text-sm text-gray-600 mt-5">
+          {paragraph} {' '}
+          <Link to={linkUrl} className="font-medium text-purple-600 hover:text-purple-500">
+              {linkName}
+          </Link>
+          </p>
+      </div>
+    )
+}
+
+export default Header
