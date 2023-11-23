c9257ba7e8ce010519bd39be96532076b51616d6
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-23 15:42:42 +0200

    add data for all social networks

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -14,6 +14,34 @@ const artistData = {
       platform: 'youtube',
       url: 'https://www.youtube.com/artistname'
     },
+    {
+      platform: 'facebook',
+      url: 'https://www.facebook.com/artistname'
+    },
+    {
+      platform: 'discord',
+      url: 'https://discord.gg/artistname'
+    },
+    {
+      platform: 'twitch',
+      url: 'https://www.twitch.tv/artistname'
+    },
+    {
+      platform: 'tiktok',
+      url: 'https://www.tiktok.com/@artistname'
+    },
+    {
+      platform: 'google',
+      url: 'https://artistname.com'
+    },
+    {
+      platform: 'x',
+      url: 'https://artistname.com'
+    },
+    {
+      platform: 'github',
+      url: 'https://github.com/artistname'
+    }
   ]
 }
 
