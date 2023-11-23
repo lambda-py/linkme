78b2e2a22a554c027e33e78c4421cf5859669e0b
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-23 16:10:39 +0200

    fix shadows and borders for logo and menu

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -55,16 +55,16 @@ function App() {
               <div className="flex justify-center items-start h-screen">
                   <img className="max-w-full h-auto max-h-[581px]" alt="Profile" src={artistData.img ? artistData.img : defaultProfileImage} />
               </div>
-              <div className="absolute w-[126px] h-[63px] top-3 left-[183px] [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] [font-family:'Outfit-ExtraBold',Helvetica] font-extrabold text-white text-[30px] text-center tracking-[0] leading-[normal]">
+              <div className="absolute w-[126px] h-[63px] top-3 left-[183px] [text-shadow:0px_0px_5px_#000000]  [font-family:'Outfit-ExtraBold',Helvetica] font-extrabold text-white text-[30px] text-center tracking-[0] leading-[normal]">
                 LinkMe
               </div>
-              <div className="absolute w-[124px] h-[63px] top-3 left-[652px] [text-shadow:0px_0px_2px_#00000040] [-webkit-text-stroke:1px_#000000] font-regular font-[number:var(--regular-font-weight)] text-white text-[length:var(--regular-font-size)] text-center tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
+              <a className="absolute w-[124px] h-[63px] top-3 left-[652px] [text-shadow:0px_0px_2px_#000000]  font-regular font-[number:var(--regular-font-weight)] text-white text-[length:var(--regular-font-size)] text-center tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
                 login
-              </div>
+              </a>
             </div>
           </div>
           <div className="absolute w-[369px] h-[491px] top-[452px] left-[505px]">
-            <div className="w-[368px] h-[70px] pt-7 [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] font-logo font-[number:var(--logo-font-weight)] text-white text-[length:var(--logo-font-size)] text-center tracking-[var(--logo-letter-spacing)] leading-[var(--logo-line-height)] [font-style:var(--logo-font-style)]">
+            <div className="w-[368px] h-[70px] pt-7 [text-shadow:0px_0px_5px_#000000] font-logo font-[number:var(--logo-font-weight)] text-white text-[length:var(--logo-font-size)] text-center tracking-[var(--logo-letter-spacing)] leading-[var(--logo-line-height)] [font-style:var(--logo-font-style)]">
               {artistData.name}
             </div>
             <SocialButtonList socials={artistData.socials} color={artistData.color} />
