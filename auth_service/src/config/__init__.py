b7ab804e1be5e7926ae2be4e361e87daef00d865
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-23 20:13:31 +0200

    clear auth service

--- auth_service/src/config/__init__.py
+++
@@ -1,5 +0,0 @@
-from auth_service.config.settings import Settings
-
-
-def get_settings() -> Settings:
-    return Settings()
--- auth_service/src/config/settings.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    service_name: str = "auth_service"
-    api_version: str = "0.1.0"
