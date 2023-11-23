2567a8a6b3315ea12a7830846d66357ca60e1202
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-23 12:41:06 +0200

    reformat buttons

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -9,67 +9,30 @@ function App() {
           <div className="absolute w-[1381px] h-[581px] top-0 left-0">
             <div className="relative w-[962px] h-[583px] top-[-2px] left-[209px]">
               <img className="absolute w-[962px] h-[581px] top-[2px] left-0" alt="Vector" src="vector.svg" />
-              <div className="absolute w-[126px] h-[63px] top-0 left-[183px] [text-shadow:0px_0px_3px_#000000] [-webkit-text-stroke:1px_#000000] [font-family:'Outfit-ExtraBold',Helvetica] font-extrabold text-white text-[30px] text-center tracking-[0] leading-[normal]">
+              <div className="absolute w-[126px] h-[63px] top-0 left-[183px] [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] [font-family:'Outfit-ExtraBold',Helvetica] font-extrabold text-white text-[30px] text-center tracking-[0] leading-[normal]">
                 LinkMe
               </div>
-              <div className="absolute w-[124px] h-[63px] top-0 left-[652px] [text-shadow:0px_4px_3px_#00000040] [-webkit-text-stroke:1px_#000000] font-regular font-[number:var(--regular-font-weight)] text-white text-[length:var(--regular-font-size)] text-center tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
+              <div className="absolute w-[124px] h-[63px] top-0 left-[652px] [text-shadow:0px_0px_2px_#00000040] [-webkit-text-stroke:1px_#000000] font-regular font-[number:var(--regular-font-weight)] text-white text-[length:var(--regular-font-size)] text-center tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
                 login
               </div>
             </div>
           </div>
           <div className="absolute w-[369px] h-[491px] top-[452px] left-[505px]">
-            <div className="absolute w-[368px] h-[70px] top-0 -left-px">
-              <div className="absolute w-[368px] h-[70px] top-[-2px] -left-px [text-shadow:0px_0px_3px_#000000] [-webkit-text-stroke:1px_#000000] font-logo font-[number:var(--logo-font-weight)] text-white text-[length:var(--logo-font-size)] text-center tracking-[var(--logo-letter-spacing)] leading-[var(--logo-line-height)] [font-style:var(--logo-font-style)]">
-                Dvi Kuli Mad
-              </div>
+            <div className="w-[368px] h-[70px] top-[-2px] -left-px [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] font-logo font-[number:var(--logo-font-weight)] text-white text-[length:var(--logo-font-size)] text-center tracking-[var(--logo-letter-spacing)] leading-[var(--logo-line-height)] [font-style:var(--logo-font-style)]">
+              Dvi Kuli Mad
             </div>
-            <div className="absolute w-[369px] h-[70px] top-[93px] left-0">
-              <button className="w-[371px] h-[70px] all-[unset] box-border">
-                <div className="relative w-[369px] h-[70px] bg-white rounded-[10px] shadow-[0px_0px_10px_#00000080]">
-                  <PlatformInstagramColorNegative color="black" className="!absolute !w-[48px] !h-[48px] !top-[11px] !left-[29px]" />
-                  <div className="absolute h-[23px] top-[24px] left-[90px] font-regular font-[number:var(--regular-font-weight)] text-black text-[length:var(--regular-font-size)] tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
-                    Instagram
-                  </div>
-                </div>
+            <div className="w-full mt-6 px-4">
+              <button className="w-full h-16 mb-3 flex items-center justify-start bg-white rounded-lg shadow-[0px_0px_8px_#00000080] px-6">
+                <PlatformInstagramColorNegative color="black" className="w-12 h-12" />
+                <span className="ml-4 text-black">
+                  Instagram
+                </span>
               </button>
-            </div>
-            <div className="absolute w-[369px] h-[70px] top-[175px] left-0">
-              <button className="w-[371px] h-[70px] all-[unset] box-border">
-                <div className="relative w-[369px] h-[70px] bg-white rounded-[10px] shadow-[0px_0px_10px_#00000080]">
-                  <PlatformYoutubeColorNegative color="black" className="!w-[48px] !top-[12px] !left-[30px] !absolute !h-[48px]" />
-                  <div className="absolute h-[23px] top-[23px] left-[91px] font-regular font-[number:var(--regular-font-weight)] text-black text-[length:var(--regular-font-size)] tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
-                    Yotube Music
-                  </div>
-                </div>
-              </button>
-            </div>
-            <div className="absolute w-[369px] h-[70px] top-[257px] left-0">
-              <button className="w-[371px] h-[70px] all-[unset] box-border">
-                <div className="relative w-[369px] h-[70px] bg-white rounded-[10px] shadow-[0px_0px_10px_#00000080]">
-                  <div className="absolute h-[23px] top-[23px] left-[91px] font-regular font-[number:var(--regular-font-weight)] text-black text-[length:var(--regular-font-size)] tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
-                    Spotify
-                  </div>
-                  {/* <SocialIcons className="!w-[48px] !top-[11px] !left-[30px] !absolute !h-[48px]" /> */}
-                </div>
-              </button>
-            </div>
-            <div className="absolute w-[369px] h-[70px] top-[339px] left-0">
-              <button className="w-[371px] h-[70px] all-[unset] box-border">
-                <div className="relative w-[369px] h-[70px] bg-white rounded-[10px] shadow-[0px_0px_10px_#00000080]">
-                  {/* <IconComponentNode className="!w-[49px] !top-[11px] !left-[30px] !absolute !h-[48px]" /> */}
-                  <div className="absolute w-[80px] h-[23px] top-[23px] left-[92px] font-regular font-[number:var(--regular-font-weight)] text-black text-[length:var(--regular-font-size)] tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
-                    Facebook
-                  </div>
-                </div>
-              </button>
-            </div>
-            <div className="absolute w-[369px] h-[70px] top-[421px] left-0">
-              <button className="w-[371px] h-[70px] all-[unset] box-border">
-                <div className="relative w-[369px] h-[70px] bg-white rounded-[10px] shadow-[0px_0px_10px_#00000080]">
-                  <div className="absolute w-[68px] h-[23px] top-[23px] left-[92px] font-regular font-[number:var(--regular-font-weight)] text-black text-[length:var(--regular-font-size)] tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
-                    LinkedIn
-                  </div>
-                </div>
+              <button className="w-full h-16 mb-3 flex items-center justify-start bg-white rounded-lg shadow-[0px_0px_8px_#00000080] px-6">
+                <PlatformYoutubeColorNegative color="black" className="w-12 h-12" />
+                <span className="ml-4 text-black">
+                  Youtube
+                </span>
               </button>
             </div>
           </div>
