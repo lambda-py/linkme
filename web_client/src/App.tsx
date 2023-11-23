3bc9d79be17c70b1f55ea3c144a8a1c3156adc54
Vitalii Hrebennikov <webasman@gmail.com>
2023-11-23 13:11:44 +0200

    fix styles, add artist img to render

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -1,24 +1,25 @@
 import { PlatformInstagramColorNegative } from "./icons/PlatformInstagramColorNegative";
 import { PlatformYoutubeColorNegative } from "./icons/PlatformYoutubeColorNegative";
+import artistPicture from "./assets/IMG_1503.jpg"
 
 function App() {
   return (
     <div className="bg-white flex flex-row justify-center w-full">
       <div className="bg-white w-[1440px] h-[1080px]">
-        <div className="relative w-[1381px] h-[943px] top-[14px] left-[31px]">
+        <div className="relative w-[1381px] h-[943px] top-0 left-[31px]">
           <div className="absolute w-[1381px] h-[581px] top-0 left-0">
             <div className="relative w-[962px] h-[583px] top-[-2px] left-[209px]">
-              <img className="absolute w-[962px] h-[581px] top-[2px] left-0" alt="Vector" src="vector.svg" />
-              <div className="absolute w-[126px] h-[63px] top-0 left-[183px] [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] [font-family:'Outfit-ExtraBold',Helvetica] font-extrabold text-white text-[30px] text-center tracking-[0] leading-[normal]">
+              <img className="w-[962px] h-[581px] top-0 left-0" alt="" src={artistPicture} />
+              <div className="absolute w-[126px] h-[63px] top-3 left-[183px] [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] [font-family:'Outfit-ExtraBold',Helvetica] font-extrabold text-white text-[30px] text-center tracking-[0] leading-[normal]">
                 LinkMe
               </div>
-              <div className="absolute w-[124px] h-[63px] top-0 left-[652px] [text-shadow:0px_0px_2px_#00000040] [-webkit-text-stroke:1px_#000000] font-regular font-[number:var(--regular-font-weight)] text-white text-[length:var(--regular-font-size)] text-center tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
+              <div className="absolute w-[124px] h-[63px] top-3 left-[652px] [text-shadow:0px_0px_2px_#00000040] [-webkit-text-stroke:1px_#000000] font-regular font-[number:var(--regular-font-weight)] text-white text-[length:var(--regular-font-size)] text-center tracking-[var(--regular-letter-spacing)] leading-[var(--regular-line-height)] [font-style:var(--regular-font-style)]">
                 login
               </div>
             </div>
           </div>
           <div className="absolute w-[369px] h-[491px] top-[452px] left-[505px]">
-            <div className="w-[368px] h-[70px] top-[-2px] -left-px [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] font-logo font-[number:var(--logo-font-weight)] text-white text-[length:var(--logo-font-size)] text-center tracking-[var(--logo-letter-spacing)] leading-[var(--logo-line-height)] [font-style:var(--logo-font-style)]">
+            <div className="w-[368px] h-[70px] pt-7 [text-shadow:0px_0px_2px_#000000] [-webkit-text-stroke:1px_#000000] font-logo font-[number:var(--logo-font-weight)] text-white text-[length:var(--logo-font-size)] text-center tracking-[var(--logo-letter-spacing)] leading-[var(--logo-line-height)] [font-style:var(--logo-font-style)]">
               Dvi Kuli Mad
             </div>
             <div className="w-full mt-6 px-4">
